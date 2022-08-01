9f5e35183976a97736de03cbb5f34545437936f6
unknown <barr520@gmail.com>
2022-08-01 21:34:10 +0100

    updated win bin

--- bin/cleanWinBuildFiles.bat
+++ bin/cleanWinBuildFiles.bat
@@ -3,4 +3,3 @@ rmdir /s /q recursivescrape.build
 rmdir /s /q recursivescrape.dist
 rmdir /s /q recursivescrape.onefile-build
 rmdir /s /q venv
-deactivate
--- bin/generateWindowsBin.bat
+++ bin/generateWindowsBin.bat
@@ -3,3 +3,4 @@ venv\bin\activate.bat
 pip install -r buildReqs.txt
 pip install -r ..\requirements.txt
 python -m nuitka ..\recursivescrape.py --onefile  --follow-stdlib -o recursivescrape.exe --assume-yes-for-downloads
+deactivate
